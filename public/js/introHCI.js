8db8338351f75aec48fc9f71a4be913b8d739758
Nathan Toung <nmtoung@gmail.com>
2014-03-01 14:25:55 -0800

    lab8

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,17 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	startTime = new Date().getTime();
+	$("#likeClick").click(likeClick);
+}
+
+function likeClick(e)
+{
+	e.preventDefault();
+	// console.log("CLICKED");	
+	ga('send', 'event', 'like', 'click');
+	
+	var endTime = new Date().getTime();
+  	var timeSpent = endTime - startTime;
+	ga('send', 'timing', 'jQuery', 'like', timeSpent);
 }
